51bfde272f4eb891fec447b5f3e99a411e9dcb04
BC <balasczerni@protonmail.com>
2026-07-29 19:25:17 +0200

    Notatki

---
+++ main.py
@@ -0,0 +1,6 @@
+# B00300S;Temperatura powietrza (oficjalna);stopień Celsjusza
+# B00604S;Suma opadu dobowego;milimetr
+# B00606S;Suma opadu godzinowego;milimetr
+
+# 1. jaka jest różnica % między Bałuty, Kwsp, Lublinek
+# 2. chcemy przerobić parametry z Bałuty i Kwsp na dobowe
